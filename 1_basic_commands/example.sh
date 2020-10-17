fda9cbc4cc2ed5e9dbd907966aceb7b1eede3cdf
CQD <cqd@hiigara.net>
2020-10-17 15:49:16 +0800

    把 zcat 改成可攜度比較高的 gunzip -c

--- 1_basic_commands/example.sh
+++ 1_basic_commands/example.sh
@@ -6,26 +6,26 @@ if [ ! -f data.gz ] ; then
 fi
 
 echo === 偷看資料 ===
-zcat < data.gz | head
+gunzip -c data.gz | head
 
 echo
 echo === 計算行數 ===
-zcat < data.gz | wc -l
+gunzip -c data.gz | wc -l
 
 echo
 echo === 檔案內有幾個不同的數字 ===
-zcat < data.gz | sort -u | wc -l
+gunzip -c data.gz | sort -u | wc -l
 
 echo
 echo === 計算數字出現的次數（節錄） ===
-zcat < data.gz | sort | uniq -c | head
+gunzip -c data.gz | sort | uniq -c | head
 
 echo
 echo === 把數字依照出現次數排序（節錄） ===
-zcat < data.gz | sort | uniq -c | sort -n -r | head
+gunzip -c data.gz | sort | uniq -c | sort -n -r | head
 
 echo
 echo === 數字出現次數的分佈狀況 ===
-zcat < data.gz | sort | uniq -c | sort -n -r | awk '{print $1}' | uniq -c
+gunzip -c data.gz | sort | uniq -c | sort -n -r | awk '{print $1}' | uniq -c
 
 echo
--- 2_awk/example.sh
+++ 2_awk/example.sh
@@ -6,29 +6,29 @@ if [ ! -f data.gz ] ; then
 fi
 
 echo === 偷看資料 ===
-zcat < data.gz | head -n 3
+gunzip -c data.gz | head -n 3
 
 echo
 echo === 計算行數 ===
-zcat < data.gz | wc -l
+gunzip -c data.gz | wc -l
 
 echo
 echo === 印出 response 大小超過 100k 的 url ===
-zcat < data.gz | awk '$10>100000{print $7}' | sort -u | head -n 3
+gunzip -c data.gz | awk '$10>100000{print $7}' | sort -u | head -n 3
 
 echo
 echo === 拉出存取量最多的 IP，然後看他在做什麼（節錄） ===
-zcat < data.gz \
+gunzip -c data.gz \
     | awk '{cnt[$1]++} END{for(ip in cnt){print ip, cnt[ip]}}' \
     | sort -k2 -n -r \
     | head -n 2 \
 
-zcat < data.gz | awk '$1=="66.249.73.135" {print $4, $7} ' | head
+gunzip -c data.gz | awk '$1=="66.249.73.135" {print $4, $7} ' | head
 
 echo
 echo === blog tag 觀看排行榜 ===
 
-zcat < data.gz \
+gunzip -c data.gz \
     | awk '$7 ~ /\/blog\/tags/{print $7}' \
     | sort \
     | uniq -c \
@@ -39,7 +39,7 @@ zcat < data.gz \
 echo
 echo === /blog/ 的每小時存取量（節錄） ===
 
-zcat < data.gz \
+gunzip -c data.gz \
     | awk '$7 ~ /\/blog/{print substr($4, 2, 15)"00"}' \
     | uniq -c \
     | awk '{print $2, $1}' | head -n 5
